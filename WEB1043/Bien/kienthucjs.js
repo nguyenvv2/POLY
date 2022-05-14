31ac7009f282386198221fb0e7f0c6d9f183487b
nguyenvv2 <nguyenvv09@gmail.com>
2022-05-14 18:44:54 +0700

    add kien thuc js

---
+++ WEB1043/Bien/kienthucjs.js
@@ -0,0 +1,45 @@
+// khai báo biến
+/*
+khai báo biến: 
+var tên_biến = value;
+let tên_biến = value;
+const tên_biến = value;
+
+nhập từ bàn phím : prompt("Mòi bạn nhập")
+chú ý prompt luôn nhập kiểu dữ liệu là string
+ví dụ x = prompt(); (giả định nhập x = 2)
+typeof a => string
+
+
+check kiểu dữ liệu của biến:
+check kiểu dữ liệu của biến dùng typeof
+kiểm tra 1 biến có phải số hay không dùng isNaN. isNaN trả ra false khi biến có giá trị 
+là số hoặc string số ví dụ false khi biến có giá trị là 2 hoặc "2". Ngược lại trả ra true
+vd: var a = 2;
+typeof a => Number
+isNaN(a) => false
+var b = "2";
+typeof b => string
+isNaN(b) => false
+var c = "Nguyen";
+typeof c => string
+isNaN(c) => true
+
+toán tử + :
+string + number => string
+number + number => number
+string số + number => string (ví dụ "2"+2 = 22)
+
+Switch - case:
+  switch (input) {
+    case 1:
+      alert("Day la thang 1");
+      break;
+    case 2:
+      alert("Day la thang 2");
+
+    default:
+      break;
+  }
+
+*/
