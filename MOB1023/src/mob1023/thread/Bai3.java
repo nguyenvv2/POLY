10a8bcc73af035961be527a66ea0ed49b3c450f6
nguyenvv2 <nguyenvv09@gmail.com>
2022-06-06 12:19:14 +0700

    update

--- MOB1023/src/mob1023/thread/Bai3.java
+++ MOB1023/src/mob1023/thread/Bai3.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package main;
+package mob1023.thread;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
